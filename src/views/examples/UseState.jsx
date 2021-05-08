a125963bf86bd4072bc90cb79ce1c94894478dbd
Luiz Henrique <58401291+MogLuiz@users.noreply.github.com>
2021-05-08 15:04:16 -0300

    Add files via upload

--- src/views/examples/UseState.jsx
+++ src/views/examples/UseState.jsx
@@ -1,16 +1,34 @@
 import React, { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
-const UseState = (props) => {
-    const x = useState
+import SectionTitle from '../../components/layout/SectionTitle'
 
-    
+const UseState = (props) => {
+    const [count, setCount] = useState(0)
+
+
     return (
         <div className="UseState">
             <PageTitle
                 title="Hook UseState"
                 subtitle="Estado em componentes funcionais!"
             />
+            <SectionTitle title="Exercicio #01" />
+            <div className="center">
+                <span className="text">{count}</span>
+                <div>
+                    <button className="btn"
+                        onClick={ () => setCount(count - 1)} > -1 </button>
+                    <button className="btn"
+                        onClick={ () => setCount(count + 1) } > +1 </button>
+                    <button className="btn"
+                        onClick={ () => setCount( valorAtual => valorAtual + 500) } > +500 </button>
+                </div>
+            </div>
+            <SectionTitle title="Exercicio #02" />
+
+            
+            
         </div>
     )
 }
